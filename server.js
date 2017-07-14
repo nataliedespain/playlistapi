7b3b195aa4ea96452422f727c97d597ba2480058
Natalie Despain <nataliedespainn@gmail.com>
2017-07-14 13:27:58 -0700

    commit

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(bodyParser.json());
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://joli-fromage-35086.herokuapp.com/api');
+    res.setHeader('Access-Control-Allow-Origin', 'nataliedespain.surge.sh');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
