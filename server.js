f94c1b3b4b01cf29434e037d7f850236a7f0bfac
Natalie Despain <nataliedespainn@gmail.com>
2017-07-14 13:21:58 -0700

    commit

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(bodyParser.json());
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+    res.setHeader('Access-Control-Allow-Origin', 'https://joli-fromage-35086.herokuapp.com/api');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
